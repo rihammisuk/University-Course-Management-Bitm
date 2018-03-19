a4369952c33a7654cfc2f12b2961efe590556c5a
rihammisuk <riham659misuk@gmail.com>
2018-03-19 21:41:24 +0600

    Bitm Project All

---
+++ UniversityMS/UniversityMS/DAL/ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace UniversityMS.DAL
+{
+    public class ConnectionString
+    {
+           public string connectionString = @"Data Source = DESKTOP-77U59OC; Database=UniversityMsDb; Integrated Security=true";
+  
+    }
+}
